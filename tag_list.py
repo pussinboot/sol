cb0198411ab5a135f96ded1f9894c4084f3b425f
Leo <leooooo@bu.edu>
2015-09-23 15:30:59 -0400

    reorganize n getting rdy 4 next steps

--- tag_list.py
+++ tag_list.py
@@ -5,16 +5,23 @@ class TagFrame(tk.Frame):
 	"""
 	frame that can hold a list of tags which can be added or deleted
 	"""
-	def __init__(self,parent,tags=[]):
+	def __init__(self,parent,clip=None,library=None):
 		tk.Frame.__init__(self,parent)
-		self.tags = tags
+		self.clip = clip
+		self.library = library
+		try:
+			self.tags = self.clip.get_tags()
+		except:
+			self.tags = []
 		self.parent = parent
-		for tag in tags:
+		tk.Label(self.parent,text="tags:").pack(anchor=tk.NW)
+
+		for tag in self.tags:
 			self.make_tag(tag).pack(side=tk.LEFT)
 		new_tag_frame = tk.Frame(self.parent,borderwidth=2, relief = tk.RIDGE)
 
 		self.new_tag_var = tk.StringVar()
-		new_tag_entry = tk.Entry(new_tag_frame,textvariable=self.new_tag_var)
+		new_tag_entry = tk.Entry(new_tag_frame,textvariable=self.new_tag_var,width=5)
 		new_tag_entry.pack(side=tk.LEFT)
 
 		def tag_adder():
@@ -32,16 +39,16 @@ class TagFrame(tk.Frame):
 		self.tags.append(tag)
 		self.make_tag(tag).pack(side=tk.LEFT)
 		# add the tag to the clip 
-		# Clip.add_tag()
+		self.clip.add_tag(tag)
 		# add the clip to the tag of the library
-		# Library.add_clip_to_tag()
+		self.library.add_clip_to_tag(self.clip,tag)
 
 	def remove_tag(self,tag):
 		self.tags.remove(tag)
 		# remove the tag from the clip
-		# Clip.remove_tag()
+		self.clip.remove_tag(tag)
 		# remove the clip from the tag of the library
-		# Library.remove_clip_from_tag()
+		self.library.remove_clip_from_tag(self.clip,tag)
 
 	def make_tag(self,tag):
 		frame = tk.Frame(self.parent,borderwidth=2, relief=tk.RIDGE)
@@ -58,6 +65,6 @@ class TagFrame(tk.Frame):
 if __name__ == '__main__':
 	root = tk.Tk()
 	root.title("tag_tester")
-	test = TagFrame(root,["tag1","tag2"])
+	test = TagFrame(root)
 	root.mainloop()
 
