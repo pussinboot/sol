6f601a65e0ca0b65ddccee8298f7bc87b4df4134
pussinboot <>
2015-10-05 23:07:22 -0400

    planning of next steps

---
+++ timeline.py
@@ -0,0 +1,22 @@
+###
+##
+# timeline
+#
+# waveform of audio file
+# w/ beat grid
+
+# playback
+# w/ loop like in traktor (2,4,8,16 bars repeated)
+
+# except 2nd part is what's cool
+# record osc/midi actions
+# so if select clip, show clip @ start of beat for ex (if snapped (which u choose w/ checkbox lol)) or at exact time u chose
+# then can adjust where within the time loop clip starts
+# on next iteration can playback or rerecord
+# each time adds a layer so you can play back diff ones and choose which is best : )
+# for now it'll just b a loop by loop kind of thing
+
+# if you r playin around w/ clip like movin it back n forth
+# plot movement on timeline
+# then can do regression
+# and play around w/ points of regression 2 make it nice n smooth 4 playback >:)
