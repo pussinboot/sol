f2eb82a3217e0bf6b9bd24f9f0c97fd95e3efb20
Leo <leooooo@bu.edu>
2015-10-07 16:01:02 -0400

    adding collections
    still workin on it

--- scratch.py
+++ scratch.py
@@ -1,5 +1,29 @@
-a = [1,2]
-a.remove(1)
-print(a)
-a.remove(2)
-print(a)
+print("poop a a 10".split()[:-1])
+
+Clips: [<__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>, <__main__.Clip object at 0x0000000002368EF0>, <__main__.Clip object at 0x0000000002383748
+>]
+[Finished in 0.2s]
