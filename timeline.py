a13e9f85bd16e2e3af6dd90689a4d91d7601d330
pussinboot <>
2015-10-16 15:31:16 -0400

    timeline fun

--- timeline.py
+++ timeline.py
@@ -20,3 +20,75 @@
 # plot movement on timeline
 # then can do regression
 # and play around w/ points of regression 2 make it nice n smooth 4 playback >:)
+
+# cool idea man
+
+import tkinter as tk
+from tkinter import ttk
+
+class Timeline:
+	def __init__(self,parent,width=1200,height=400):
+		# main elements
+		self.parent = parent
+		self.canvas = tk.Canvas(parent,width=width,height=height,bg="black")
+		self.canvas.pack()
+		# variables
+		self.grid = []
+		self.last_point = (10,height/2)
+		self.dx = 4
+		self.width, self.height = width, height
+		self.draw_grid()
+		self.canvas.bind( "<B1-Motion>", self.integrate_click )
+		self.canvas.bind("<ButtonPress-3>",self.reset)
+		#self.canvas.bind( "<B1-Motion>", self.paint )
+		#self.canvas.bind( "<ButtonPress-1>", self.add_point )
+	def reset(self,event):
+		self.canvas.delete("all")
+		self.last_point = (10,self.height/2)
+		self.draw_grid()
+
+	def draw_grid(self,nX=50,nY=20):
+		self.grid = []
+		self.grid.append(self.canvas.create_line(10,self.height/2,
+						 self.width, self.height/2,
+						 arrow="last", fill="green"))
+		self.grid.append(
+						 self.canvas.create_line(10, self.height - 5,
+						 10, 5,
+						 arrow="last", fill="green"))
+
+		numberX = int((self.width - 10) / nX)
+		numberY = int((self.height - 5) / nY)
+
+		for i in range(1, numberX + 1):
+			line = self.canvas.create_line(i*nX,0,i*nX,self.height,fill='gray')
+			self.grid.append(line)
+
+		for i in range(1, (numberY + 1)//2):
+			self.grid.append(self.canvas.create_line(10, (self.height/2)+i*nY,self.width, (self.height/2)+i*nY,fill='gray'))
+			
+			self.grid.append(self.canvas.create_line(10, (self.height/2)-i*nY,self.width, (self.height/2)-i*nY,fill='gray'))
+
+	def paint(self, event ):
+		python_green = "#476042"
+		r = 5
+		x1, y1 = ( event.x - r ), ( event.y - r )
+		x2, y2 = ( event.x + r ), ( event.y + r )
+		self.canvas.create_oval( x1, y1, x2, y2, fill = python_green )
+
+	def add_point(self, event):
+		new_point = (event.x, event.y)
+		self.canvas.create_line(self.last_point[0],self.last_point[1],new_point[0],new_point[1],fill='white')
+		self.last_point = new_point
+
+	def integrate_click(self,event):
+		new_point = (self.last_point[0]+self.dx, event.y)
+		self.canvas.create_line(self.last_point[0],self.last_point[1],new_point[0],new_point[1],fill='white')
+		self.last_point = new_point
+
+if __name__ == '__main__':
+	
+	root = tk.Tk()
+	root.title("timeline_test")
+	timeline = Timeline(root)
+	root.mainloop()
