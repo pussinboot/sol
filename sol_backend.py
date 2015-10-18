d549d2dadf5a3b14b6fdec1d2e352fd34784b11f
pussinboot <>
2015-10-18 14:53:38 -0400

    alternative threading

--- sol_backend.py
+++ sol_backend.py
@@ -1,10 +1,11 @@
 # backend for midi/osc stuff
-from osc import Client
+from osc import Client,Server
 
 class Backend:
 
 	def __init__(self):
 		self.client = Client()
+		self.server = Server()
 
 if __name__ == '__main__':
 	test_b = Backend()
