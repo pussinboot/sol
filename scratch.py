1a4787bbafdf6843b95a2822d8bdf3847980f827
pussinboot <>
2015-11-03 22:19:06 -0500

    oo ok got it now

--- scratch.py
+++ scratch.py
@@ -1,6 +1,16 @@
-name = "11112121"
+last_point = (0,0)
+dx = 1
 
-print(name.split("_")[:-1])
-print(None)
-x = []
-if x: print('y')
+def add_alot_of_midi(ns):
+	global last_point
+	global dx
+	points = [last_point[0],last_point[1]]
+	for i in range(len(ns)):
+		#to_add = [,]
+		#print(type(to_add))
+		points.append(points[2*i]+dx)
+		points.append(points[2*i+1]+ns[i])
+	print(*points)
+	last_point = points[-2:]
+
+add_alot_of_midi([1,2,3,4,5])
