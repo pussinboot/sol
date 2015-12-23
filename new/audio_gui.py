3e89c3d240e5771a0ee34f09676131d3f4004e04
pussinboot <>
2015-12-23 00:22:42 -0500

    audio_gui start

---
+++ new/audio_gui.py
@@ -0,0 +1,9 @@
+from pythonosc import dispatcher
+from pythonosc import osc_server
+import threading
+
+# gui should be like
+# file -> open
+# pause. play, start from beginning, stop
+# big rect with bar going across it to indicate position that can be clicked on or dragged to set pos
+# optional cool spectro
