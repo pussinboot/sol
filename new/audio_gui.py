6c767891da1594530cdbff93199a0124dfd3841f
Leo <leooooo@bu.edu>
2015-12-23 16:26:16 -0500

    audio_gui can send and receive osc messages

--- new/audio_gui.py
+++ new/audio_gui.py
@@ -14,28 +14,40 @@ import threading
 
 class MainGui:
 	def __init__(self,root):
-		# general
-		self.queue = None
 		# gui
 		self.root = root
 		self.frame = tk.Frame(root)
 		self.control_frame = tk.Frame(self.frame)
 		self.progress_frame = tk.Frame(self.frame)
+
 		self.setup_buttons()
 
-		self.control_frame.pack()
+		self.progress_var = tk.StringVar()
+		self.progress_var.set("--:--")
+		self.progress_time = tk.Label(self.progress_frame,textvariable=self.progress_var)
+		self.progress_time.pack()
+
+		self.control_frame.pack(side=tk.LEFT)
 		self.progress_frame.pack()
 		self.frame.pack()
 		# osc
 		self.osc_server = OscControl(self)
 		self.osc_client = udp_client.UDPClient("127.0.0.1", 7007)
 
+		self.osc_start()
+
 	def osc_start(self):
-		self.queue = queue.Queue()
-		self.osc_server.queue = self.queue
 		# osc mappings (inputs)
-		self.osc_server.dispatcher.map("/pyaud/pos/sec",print)
+		def sec_to_str(_,osc_msg):
+			try:
+				time_sec = float(osc_msg)
+				self.progress_var.set("%d:%02d"  %  (time_sec // 60.0 , time_sec % 60.0))
+			except:
+				self.progress_var.set("--:--")
+
+		self.osc_server.dispatcher.map("/pyaud/pos/sec",sec_to_str)
 		self.osc_server.start()
+
 	def setup_buttons(self):
 		# osc msgs
 		def build_msg(addr,arg):
@@ -59,10 +71,9 @@ class MainGui:
 							command = gen_osc_send(pause))
 		stopbut =  tk.Button(self.control_frame,text="[]",padx=10,pady=10,relief=tk.GROOVE,
 							command = gen_osc_send(stop))
-		playbut.pack()
+		playbut.pack(side=tk.LEFT)
 		pawsbut.pack()
 		stopbut.pack()
-
 
 class OscControl:
 	def __init__(self,gui=None,server_ip="127.0.0.1",server_port=7008):
@@ -71,41 +82,19 @@ class OscControl:
 		self.refresh_int = 25
 		self.server_ip, self.server_port = server_ip,server_port
 		self.dispatcher = dispatcher.Dispatcher()
-		#self.dispatcher.map("/midi",self.put_in_queue)
 
-		
-		#self.server_thread.start()
-
-	def put_in_queue(self,_,value):
-		#arr = eval(value)
-		#tor = (arr[:2],arr[2])
-		#print(tor)
-		self.queue.put(value)
-	
 	def start(self):
 		self.running = 1
 		self.gui.root.protocol("WM_DELETE_WINDOW",self.stop)
 		self.server = osc_server.ThreadingOSCUDPServer((self.server_ip, self.server_port), self.dispatcher)
 		self.server_thread = threading.Thread(target=self.server.serve_forever)
 		self.server_thread.start()
-		self.run_periodic = self.gui.root.after(self.refresh_int,self.periodicCall)
 
 	def stop(self):
 		self.running = 0
 		self.server.shutdown()
 		self.server_thread.join()
-
-# thread to update gui
-
-	def periodicCall(self):
-		self.gui.processIncoming()
-		if not self.running:
-			if self.run_periodic:
-				self.gui.root.after_cancel(self.run_periodic)
-				self.gui.quit()
-				self.gui.root.destroy()
-
-		self.run_periodic = self.gui.root.after(self.refresh_int,self.periodicCall)
+		self.gui.root.destroy()
 
 if __name__ == '__main__':
 	root = tk.Tk()
