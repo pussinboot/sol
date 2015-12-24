5a83a18fddc094da5bc79ce2cd2444ea327d199c
Leo <leooooo@bu.edu>
2015-12-24 14:09:07 -0500

    progress_bar complete

--- new/audio_gui.py
+++ new/audio_gui.py
@@ -1,6 +1,11 @@
 from pythonosc import osc_message_builder, udp_client, dispatcher, osc_server
+
 import tkinter as tk
-import threading
+import tkinter.filedialog as tkfd
+import tkinter.messagebox as tkmb
+from tkinter import ttk
+
+import threading,os
 
 # gui should be like
 # file -> open
@@ -11,6 +16,12 @@ import threading
 # has osc server and client
 # server to receive playback status
 # client to control the audio_server
+
+def build_msg(addr,arg):
+	msg = osc_message_builder.OscMessageBuilder(address = addr)
+	msg.add_arg(arg)
+	msg = msg.build()
+	return msg
 
 class MainGui:
 	def __init__(self,root):
@@ -27,13 +38,15 @@ class MainGui:
 		self.progress_time = tk.Label(self.progress_frame,textvariable=self.progress_var)
 		self.progress_time.pack()
 
-		self.control_frame.pack(side=tk.LEFT)
+		self.control_frame.pack(side=tk.LEFT,anchor=tk.W,expand=True,fill=tk.X)
 		self.progress_frame.pack()
-		self.frame.pack()
+		self.frame.pack(expand=True,fill=tk.BOTH)
 		# osc
 		self.osc_server = OscControl(self)
 		self.osc_client = udp_client.UDPClient("127.0.0.1", 7007)
 
+		self.setup_menu()
+		self.progress_bar = ProgressBar(self)
 		self.osc_start()
 
 	def osc_start(self):
@@ -44,17 +57,19 @@ class MainGui:
 				self.progress_var.set("%d:%02d"  %  (time_sec // 60.0 , time_sec % 60.0))
 			except:
 				self.progress_var.set("--:--")
-
+		def float_to_prog(_,osc_msg):
+			try:
+				float_perc = float(osc_msg)
+				self.progress_bar.move_bar(float_perc*self.progress_bar.width)
+			except:
+				self.progress_bar.move_bar(0)
 		self.osc_server.dispatcher.map("/pyaud/pos/sec",sec_to_str)
+		self.osc_server.dispatcher.map("/pyaud/pos/float",float_to_prog)
+		self.osc_server.dispatcher.map("/pyaud/status",self.buttons_enabler)
 		self.osc_server.start()
 
 	def setup_buttons(self):
 		# osc msgs
-		def build_msg(addr,arg):
-			msg = osc_message_builder.OscMessageBuilder(address = addr)
-			msg.add_arg(arg)
-			msg = msg.build()
-			return msg
 
 		play = build_msg('/pyaud/pps',1)
 		pause = build_msg('/pyaud/pps',0)
@@ -65,15 +80,80 @@ class MainGui:
 				self.osc_client.send(msg)
 			return sender
 
-		playbut =  tk.Button(self.control_frame,text=">",padx=10,pady=10,relief=tk.GROOVE,
-							command = gen_osc_send(play))
-		pawsbut =  tk.Button(self.control_frame,text="||",padx=10,pady=10,relief=tk.GROOVE,
-							command = gen_osc_send(pause))
-		stopbut =  tk.Button(self.control_frame,text="[]",padx=10,pady=10,relief=tk.GROOVE,
-							command = gen_osc_send(stop))
-		playbut.pack(side=tk.LEFT)
-		pawsbut.pack()
-		stopbut.pack()
+		self.playbut =  tk.Button(self.control_frame,text=">",padx=8,pady=4,
+							command = gen_osc_send(play),state='disabled')
+		self.pausebut =  tk.Button(self.control_frame,text="||",padx=8,pady=4,
+							command = gen_osc_send(pause),state='disabled')
+		def stopfun():
+			osc_send = gen_osc_send(stop)
+			osc_send()
+			self.progress_var.set("--:--")
+		self.stopbut =  tk.Button(self.control_frame,text="[]",padx=8,pady=4,
+							command = stopfun)
+		self.playbut.pack(side=tk.LEFT)
+		self.pausebut.pack(side=tk.LEFT)
+		self.stopbut.pack(side=tk.LEFT)
+
+	def buttons_enabler(self,_,osc_msg):
+		if osc_msg == 'loaded' or osc_msg == 'paused':
+			self.playbut.config(state='normal')
+			self.pausebut.config(state='disabled')
+		elif osc_msg == 'playing':
+			self.playbut.config(state='disabled')
+			self.pausebut.config(state='normal')
+		elif osc_msg == 'stopped':
+			self.playbut.config(state='disabled')
+			self.pausebut.config(state='disabled')
+
+	def setup_menu(self):
+		self.menubar = tk.Menu(self.root)
+		self.filemenu = tk.Menu(self.menubar,tearoff=0)
+		self.filemenu.add_command(label="open",command=self.open_file)
+		self.filemenu.add_command(label="quit",command=self.osc_server.stop)
+		self.menubar.add_cascade(label='file',menu=self.filemenu)
+
+		self.root.config(menu=self.menubar)
+
+	def open_file(self):
+		filename = tkfd.askopenfilename(parent=root,title='Choose your WAV file')
+		splitname = os.path.splitext(filename)
+		if splitname[1] == '.wav' or splitname[1] == '.WAV':
+			self.osc_client.send(build_msg('/pyaud/open',filename))
+		else:
+			tkmb.showerror("Bad file","please choose a proper .wav file")
+
+class ProgressBar:
+	def __init__(self,parent,width=200,height=50):
+
+		self.width, self.height = width, height
+
+		self.parent = parent
+		self.root = parent.root
+		self.frame = tk.Frame(root)
+		self.canvas = tk.Canvas(self.frame,width=width,height=height,bg="black",scrollregion=(0,0,width,height))
+
+		self.hbar = tk.Scrollbar(self.frame,orient=tk.HORIZONTAL)
+		self.hbar.config(command=self.canvas.xview)
+		self.canvas.config(xscrollcommand=self.hbar.set)
+		self.hbar.pack(side=tk.BOTTOM,fill=tk.X)
+
+		self.pbar = self.canvas.create_line(0,0,0,height,fill='gray',width=3)
+
+		self.canvas.bind("<B1-Motion>",self.find_mouse)
+		self.canvas.bind("<ButtonRelease-1>",self.find_mouse)
+		self.canvas.pack()
+		self.frame.pack()
+
+	def find_mouse(self,event):
+		#print(event.x, event.y)
+		self.move_bar(event.x)
+		msg = build_msg('/pyaud/seek/float',event.x/self.width)
+		self.parent.osc_client.send(msg)
+
+	def move_bar(self,new_x):
+		self.canvas.coords(self.pbar,new_x,0,new_x,self.height)
+
+
 
 class OscControl:
 	def __init__(self,gui=None,server_ip="127.0.0.1",server_port=7008):
