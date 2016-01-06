5a5dd340cfa331d87e2dda1852f7b7fb1d479d06
pussinboot <>
2016-01-05 23:50:57 -0500

    all the parts are there, now just gotta make it epic

--- new/midi_control.py
+++ new/midi_control.py
@@ -5,7 +5,7 @@ and allows for configuring midi
 """
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
-import pygame.midi, threading
+import pygame.midi, threading, queue
 from sol_backend import Backend
 
 class Midi2Osc:
@@ -15,7 +15,8 @@ class Midi2Osc:
 		if inp:
 			self.inp = pygame.midi.Input(inp)
 		else:
-			self.inp = pygame.midi.Input(pygame.midi.get_default_input_id())
+			self.inp = None
+		self.outp = None
 		# osc
 		self.client = udp_client.UDPClient(ip,port)
 		self.going = False
@@ -36,14 +37,51 @@ class Midi2Osc:
 			i += 1
 		return [inputs,outputs]
 
-	def change_inp(self,newimp):
-		self.inp.close()
+	def change_inp(self,newimp=None):
+		if self.inp: self.inp.close()
+		def default():
+			try:
+				self.inp = pygame.midi.Input(pygame.midi.get_default_input_id())
+				return pygame.midi.get_default_input_id()
+			except:
+				# no inputs
+				return -1
+		if not newimp:
+			return default()
 		try:
 			self.inp = pygame.midi.Input(newimp)
 			return newimp
 		except:
-			self.inp = pygame.midi.Input(pygame.midi.get_default_input_id())
-			return pygame.midi.get_default_input_id()
+			return default()
+
+	def change_outp(self,newoutp=None):
+		if self.outp: self.outp.close()
+		def default():
+			try:
+				self.outp = pygame.midi.Output(pygame.midi.get_default_output_id())
+				return pygame.midi.get_default_output_id()
+			except:
+				# no outputs
+				return -1
+		if not newoutp: 
+			return default()
+		try:
+			self.outp = pygame.midi.Output(newoutp)
+			return newoutp
+		except Exception as e:
+			print(e)
+			return default()
+
+	def send_output(self,note,channel,on_off=True,velocity=None):
+		if not self.outp:
+			return
+		try:
+			if on_off:
+				self.outp.note_on(note,velocity,channel)
+			else:
+				self.outp.note_off(note,velocity,channel)
+		except Exception as e:
+			print('oops',e)
 
 	def start(self):
 		self.going = True
@@ -53,7 +91,8 @@ class Midi2Osc:
 	def stop(self):
 		self.going = False
 		if self.server_thread: self.server_thread.join()
-		self.inp.close()
+		if self.inp: self.inp.close()
+		if self.outp: self.outp.close()
 		pygame.midi.quit()
 
 	def serve(self):
@@ -62,8 +101,8 @@ class Midi2Osc:
 				midi_events = self.inp.read(10)
 				#the_key = str([midi_events[0][0][0],midi_events[0][0][1]])
 				#n = int(midi_events[0][0][2])
-				print(midi_events[0][0][:3])#the_key,n)
-				msg = osc_message_builder.OscMessageBuilder(address = "/midi")
+				#print(midi_events[0][0][:3])#the_key,n)
+				msg = osc_message_builder.OscMessageBuilder(address = "/midi/test")
 				msg.add_arg(str(midi_events[0][0][:3]))
 				msg = msg.build()
 				self.client.send(msg)
@@ -72,20 +111,80 @@ class ConfigMidi:
 	def __init__(self,backend,configfile='/savedata/last_midi'):
 		self.backend = backend
 		self.configfilename = configfile
-		self.midi2osc = Midi2Osc(ip=self.backend.osc_server.ip,port=self.backend.osc_server.port)
+		self.m2o = Midi2Osc(ip=self.backend.osc_server.ip,port=self.backend.osc_server.port)
+		self.queue = queue.Queue()
+
+		def osc_to_id(_,osc_msg):
+			msg = eval(osc_msg)
+			self.queue.put([str(msg[:2]),msg[2]])
+			#key, val = str(msg[:2]),msg[2]
+			#if key in self.key_to_fun:
+			#	resp = self.key_to_fun[key](val)
+			#	if resp: self.queue.put(resp)
+		
+		self.backend.osc_server.map('/midi/test',osc_to_id)
+
+	def select_devices(self,inp=None,outp=None):
+		return [self.m2o.change_inp(inp),self.m2o.change_outp(outp)]
+
+	def start(self):
+		self.backend.osc_server.start()
+		self.m2o.start()	
+
 	def stop(self):
-		self.midi2osc.stop()
+		self.backend.osc_server.stop()
+		self.m2o.stop()
+
+	def id_midi(self):
+		# find most common key, look @ ns, figure out wat kind of key it is : )
+		msgs = []
+		while self.queue.qsize():
+			try:
+				msg = self.queue.get(0)
+				msgs.append(msg)
+			except queue.Empty:
+				pass
+		msgs = msgs[-10:]
+		f=lambda s,d={}:([d.__setitem__(i[0],d.get(i[0],0)+1) for i in s],d)[-1]
+		hist = f(msgs)
+		def keywithmaxval(d):
+			v=list(d.values())
+			k=list(d.keys())
+			return k[v.index(max(v))]
+		if hist:
+			maxval = keywithmaxval(hist)
+			ns = [msgs[i][1] for i, x in enumerate(msgs) if x[0] == maxval]
+			f=lambda s,d={}:([d.__setitem__(i,d.get(i,0)+1) for i in s],d)[-1]
+			hist_ns = f(ns)
+			return [maxval,len(hist_ns)] # returns midi key and number of different values it took on
+			# for example if it is [[],2] then [] is a piano type control
+
+			
 
 if __name__ == '__main__':
+	import time
+
 	### test Midi2Osc
-	# m2o_test = Midi2Osc()
-	# m2o_test.start()
-	# import time
-	# time.sleep(1)
-	# m2o_test.stop()
+	m2o_test = Midi2Osc()
+	m2o_test.change_inp()
+	m2o_test.change_outp(3)
+	m2o_test.start()
+	channel = 7
+	for _ in range(4):
+		for note in range(96,104):
+			m2o_test.send_output(note,channel,velocity=127)
+			time.sleep(.1)
+			m2o_test.send_output(note,channel,False)
+	m2o_test.stop()
 	
 	### test ConfigMidi
-	bb = Backend('../old/test.avc')
-	cm_test = ConfigMidi(bb)
-	print(cm_test.midi2osc.get_inps())
-	cm_test.stop()
+	# bb = Backend('../old/test.avc')
+	# cm_test = ConfigMidi(bb)
+	# print(cm_test.m2o.get_inps())
+	# print('default i/o',cm_test.select_devices())
+	# cm_test.start()
+	# for i in range(4):
+	# 	print('going to test iding an input now')
+	# 	time.sleep(1)
+	# 	print(cm_test.id_midi())
+	# cm_test.stop()
