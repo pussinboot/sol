b0507bb9a00f70f9c160347aca2909fd93e28ada
Leo <leooooo@bu.edu>
2016-07-19 15:35:47 -0400

    renaimng

--- sol/database/database_test.py
+++ sol/database/database_test.py
@@ -12,3 +12,6 @@ assert testdb.search('test')[0].f_name == test_fnames[1]
 
 testdb.remove_clip(testdb.clips[test_fnames[2]])
 assert len(testdb.search('cool')) == 0
+
+testdb.rename_clip(testdb.clips[test_fnames[0]],'nop not funny')
+assert testdb.search('nop')[0].f_name == test_fnames[0]
