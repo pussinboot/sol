7554761091ee883443964804394b1722785b9615
Leo <leooooo@bu.edu>
2016-07-25 14:07:23 -0400

    the brains are being moved
    :fallen_leaf:

---
+++ sol/magi.py
@@ -0,0 +1,9 @@
+class Magi:
+	"""
+	keep track of library, current collection, etc
+	& control everything =)
+	2 paths
+	input -> action      | internal state -> action
+	setup midi, osc etc. | keep track of vars
+	                     | for looping,
+	                     | updating of library
