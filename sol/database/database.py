771d3a6891779073b8da33580f3340ffef99f827
pussinboot <>
2016-07-28 11:22:36 -0400

    clip collection save/load gud

--- sol/database/database.py
+++ sol/database/database.py
@@ -185,14 +185,14 @@ class FileOPs:
 		# this necessitates loading all clips before looking at clip collections
 		col_element = ET.Element('clip_collection')
 
-		clip_element.set('n',len(clip_col.clips)) 
-		clip_element.set('name',clip_col.name) 
+		col_element.set('n',str(len(clip_col.clips))) 
+		col_element.set('name',clip_col.name) 
 
-		clips = ET.SubElement(clip_element,'clips')
+		clips = ET.SubElement(col_element,'clips')
 		for clip in clip_col.clips:
-			# if clip is not none
-			clip = ET.SubElement(clips,'clip')
-			clip.text = clip.f_name
+			clip_el = ET.SubElement(clips,'clip')
+			if clip is not None:
+				clip_el.text = clip.f_name
 	
 		return col_element
 
@@ -202,7 +202,19 @@ class FileOPs:
 		# n, name from the top element
 		# then each line is ith clip
 		# (loaded from clips_from_db if it exists)
-		pass
+		n = eval(col_element.get('n'))
+		name = col_element.get('name')
+
+		if n is None or name is None:
+			return
+		clip_sub_elms = [child for child in col_element[0]]
+		clip_names = [c.text for c in clip_sub_elms]
+
+		clip_col_tor = ClipCollection(n,name)
+		for i in range(n):
+			if clip_names[i] in clips_from_db:
+				clip_col_tor[i] = clips_from_db[clip_names[i]]
+		return clip_col_tor
 
 
 if __name__ == '__main__':
@@ -219,27 +231,40 @@ if __name__ == '__main__':
 	# print(testdb.search('bazin')[0])
 	# testclip = testdb.clips[test_fnames[1]]
 
-	test_params = {
-		'queue_points'   : [0.01,0.51,None,0.76,None,None,None,None],
-		'loop_points'    : [(0.01,0.51,'d'),(0.6,0.7,'b'),None,None],
-		'loop_selection' : 0,
-		'loop_on'        : True,
-		'playback_speed' : 5.2,
-		'play_direction' : 'p',
-		'control_speed'  : 3.33
-	}
+	# test_params = {
+	# 	'queue_points'   : [0.01,0.51,None,0.76,None,None,None,None],
+	# 	'loop_points'    : [(0.01,0.51,'d'),(0.6,0.7,'b'),None,None],
+	# 	'loop_selection' : 0,
+	# 	'loop_on'        : True,
+	# 	'playback_speed' : 5.2,
+	# 	'play_direction' : 'p',
+	# 	'control_speed'  : 3.33
+	# }
 
-	testclip = Clip("test_clip.mov","/fake/act",params=test_params,
-		tags = ['test_tag1','tag2','tag2','teststst'])
+	# testclip = Clip("test_clip.mov","/fake/act",params=test_params,
+	# 	tags = ['test_tag1','tag2','tag2','teststst'])
 
-	xmlclip = testdb.file_ops.save_clip(testclip)
-	# ET.ElementTree(xmlclip).write('./testclip.xml')
-	rough_string = ET.tostring(xmlclip, 'utf-8')
+	# xmlclip = testdb.file_ops.save_clip(testclip)
+	# # ET.ElementTree(xmlclip).write('./testclip.xml')
+	# rough_string = ET.tostring(xmlclip, 'utf-8')
+	# reparsed = minidom.parseString(rough_string)
+	# print(reparsed.toprettyxml(indent="\t"))
+	# # print(ET.dump(xmlclip))
+	# test_testclip = testdb.file_ops.load_clip(xmlclip)
+	# print(testclip)
+	# print(test_testclip)
+	# print([testclip,test_testclip])
+	# print(test_testclip.params)
+
+	test_col = ClipCollection()
+	for i in range(len(test_fnames)):
+		test_col[i] = testdb.clips[test_fnames[i]]
+	testclipcol = testdb.file_ops.save_clip_col(test_col)
+	rough_string = ET.tostring(testclipcol, 'utf-8')
 	reparsed = minidom.parseString(rough_string)
 	print(reparsed.toprettyxml(indent="\t"))
-	# print(ET.dump(xmlclip))
-	test_testclip = testdb.file_ops.load_clip(xmlclip)
-	print(testclip)
-	print(test_testclip)
-	print([testclip,test_testclip])
-	print(test_testclip.params)
+	test_parsed_col = testdb.file_ops.load_clip_col(testclipcol,testdb.clips)
+	print(len(test_parsed_col))
+	for i in range(8):
+		print(test_parsed_col[i])
+	print(test_parsed_col[2] == testdb.clips[test_fnames[2]])
