e5bf42577a2138203c5fa12f18bcf1d814f94910
pussinboot <>
2016-07-29 16:59:30 -0400

    collection funs

--- tests/scratch.py
+++ tests/scratch.py
@@ -1,16 +1,1 @@
-list_of_str = ['test1','twoo','therere']
-newstr = ','.join(list_of_str)
-print(newstr)
-print(newstr.split(','))
-teststr = ''
-print(teststr.split(','))
-
-
-test_dict = {
-	'a':1,
-	'b':222,
-	'c':'noob'
-}
-
-for thing in test_dict.values():
-	print(thing)
+print(-1 % 7)
