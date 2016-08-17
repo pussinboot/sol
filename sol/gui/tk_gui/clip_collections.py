5b54b9d2e8baf409a1fcc96d6042e1cd9186e44b
pussinboot <>
2016-08-17 17:04:10 -0400

    work on clip org gui.. not done yet

--- sol/gui/tk_gui/clip_collections.py
+++ sol/gui/tk_gui/clip_collections.py
@@ -526,4 +526,134 @@ class LibraryBrowser:
 				self.browse_tree.insert(cur_folder, 'end', text=files[i][1],values=["clip",files[i][2]])
 
 
+class ClipOrg:
+	# popup with the clip org gui =)
+	# ClipCont = ClipContainer but with unbind("<Double-3>")
+	def __init__(self,root,parent):
+		self.root = root
+		self.parent = parent
+		self.backend = self.parent.magi
+		self.root.title('clip org')
+		self.parent.root.call('wm', 'attributes', '.', '-topmost', '0')
+		self.root.protocol("WM_DELETE_WINDOW",self.close)		
+		self.index = -1
+
+		self.mainframe = tk.Frame(root)
+		self.clip_frame = ttk.Notebook(self.mainframe)
+		self.all_clip_frame = ttk.Frame(self.clip_frame)
+		self.all_clip_canvas = tk.Canvas(self.all_clip_frame)
+		self.frame = self.all_clip_inner_frame = tk.Frame(self.all_clip_canvas)
+		self.vsb_all_clips = tk.Scrollbar(self.all_clip_frame, orient="vertical", command=self.all_clip_canvas.yview)
+		self.all_clip_canvas.configure(yscrollcommand=self.vsb_all_clips.set)
+		self.clip_frame.add(self.all_clip_frame,text='all clips')
+
+		self.search_result_frame = ttk.Frame(self.clip_frame)
+		self.search_clip_canvas = tk.Canvas(self.search_result_frame)
+		self.search_clip_inner_frame = tk.Frame(self.search_clip_canvas)
+
+		self.search_clip_inner_frame.frame = self.search_clip_inner_frame
+		self.search_clip_inner_frame.mouse_wheel = self.mouse_wheel_search
+		self.search_clip_inner_frame.bind("<MouseWheel>", self.mouse_wheel_search)
+		self.search_clip_inner_frame.bind("<Button-4>", self.mouse_wheel_search)
+		self.search_clip_inner_frame.bind("<Button-5>", self.mouse_wheel_search)
+		self.search_clip_inner_frame.backend = self.backend
+
+		self.vsb_search_clips = tk.Scrollbar(self.search_result_frame, orient="vertical", command=self.search_clip_canvas.yview)
+		self.search_clip_canvas.configure(yscrollcommand=self.vsb_search_clips.set)
+		self.clip_frame.add(self.search_result_frame,text='search')
+
+		self.search_query = tk.StringVar()
+		self.search_field = tk.Entry(self.search_result_frame,textvariable=self.search_query)
+		self.search_query.trace('w',self.search)
+
+		self.clip_conts = []
+		self.clip_folds = []
+		self.search_res_clip_conts = []
+
+		self.mainframe.pack(expand=True,fill=tk.Y)
+		self.clip_frame.pack(expand=True,fill="both")
+		self.vsb_all_clips.pack(side="right", fill="y")
+		self.all_clip_canvas.pack(side="left", fill="both", expand=True)
+		self.all_clip_canvas.create_window((4,4), window=self.all_clip_inner_frame, anchor="nw", 
+								  tags="self.all_clip_inner_frame")
+		self.all_clip_canvas.bind("<MouseWheel>", self.mouse_wheel)
+		self.all_clip_canvas.bind("<Button-4>", self.mouse_wheel)
+		self.all_clip_canvas.bind("<Button-5>", self.mouse_wheel)
+
+		self.all_clip_inner_frame.bind("<Configure>", self.reset_scroll_region)
+
+		self.initialize_all_clips()
+
+		self.search_field.pack(side="top",fill="x")
+		self.vsb_search_clips.pack(side="right", fill="y")
+		self.search_clip_canvas.pack(side="left", fill="both", expand=True)
+		self.search_clip_canvas.create_window((4,4), window=self.search_clip_inner_frame, anchor="nw", 
+								  tags="self.search_clip_inner_frame")
+		self.search_clip_canvas.bind("<MouseWheel>", self.mouse_wheel_search)
+		self.search_clip_canvas.bind("<Button-4>", self.mouse_wheel_search)
+		self.search_clip_canvas.bind("<Button-5>", self.mouse_wheel_search)
+
+	def reset_scroll_region(self, event=None):
+		self.all_clip_canvas.configure(scrollregion=self.all_clip_canvas.bbox("all"))
+		self.search_clip_canvas.configure(scrollregion=self.search_clip_canvas.bbox("all"))
+
+	def mouse_wheel(self,event):
+		 self.all_clip_canvas.yview('scroll',-1*int(event.delta//120),'units')
+
+	def mouse_wheel_search(self,event):
+		 self.search_clip_canvas.yview('scroll',-1*int(event.delta//120),'units')
+
+	def initialize_all_clips(self):
+		# first sort by filename : )
+		all_clips = self.backend.db.alphabetical_listing
+		
+		for i, cc in enumerate(all_clips):
+			new_clip_cont = ClipContainer(self,-1,self.backend.select_clip,cc[1])
+			new_clip_cont.label.unbind('<Double-3>')
+			new_clip_cont.grid(row=i//4,column=i%4)
+
+		# for i in range(len(fnames)):
+		# 	foldername = fnames[i].split("\\")[-2]
+		# 	if foldername == 'dxv':
+		# 		foldername = fnames[i].split("\\")[-3]
+		# 	if foldername != last_folder_name:
+		# 		offset = i
+		# 		last_folder_name = foldername
+		# 		new_frame = tk.LabelFrame(self.all_clip_inner_frame,text=foldername)
+		# 		new_frame.frame = new_frame
+		# 		new_frame.mouse_wheel = self.mouse_wheel
+		# 		new_frame.bind("<MouseWheel>", self.mouse_wheel)
+		# 		new_frame.bind("<Button-4>", self.mouse_wheel)
+		# 		new_frame.bind("<Button-5>", self.mouse_wheel)
+		# 		new_frame.backend = self.backend
+		# 		self.clip_folds.append(new_frame)
+		# 	newcont = ClipCont(self.backend.library.clips[fnames[i]],self.lib_gui,self.clip_folds[-1])
+		# 	self.clip_conts.append(newcont)
+		# 	self.clip_conts[-1].grid(row=((i-offset)//self.across),column=((i-offset)%self.across))
+
+		# for frame in self.clip_folds:
+		# 	frame.pack()
+
+	def search(self,event,*args):
+		# print('searching',self.search_query.get())
+		search_term = self.search_query.get()
+		for cont in self.search_res_clip_conts:
+			cont.frame.grid_forget()
+			cont.frame.destroy()
+		self.search_res_clip_conts = []
+		if search_term != "":
+			res = self.backend.search.by_prefix(search_term)
+			fnames = [r for r in res if r in self.backend.library.clips]
+			for i in range(len(fnames)):
+				newcont = ClipCont(self.backend.library.clips[fnames[i]],self.lib_gui,self.search_clip_inner_frame)
+				self.search_res_clip_conts.append(newcont)
+				self.search_res_clip_conts[-1].grid(row=((i)//self.across),column=((i)%self.across))
+		self.reset_scroll_region()
+	def quit(self):
+		self.backend.save_data()
+
+
+	def close(self,*args):
+		self.parent.root.call('wm', 'attributes', '.', '-topmost', str(int(self.parent.on_top_toggle.get())))
+		self.root.destroy()
 
