980de6c8a8b88ab5944ea80d69e1e7a336dcc0d3
pussinboot <>
2017-04-24 11:12:43 -0400

    ffmpeg paths thumbnail fix

--- sol/database/thumbs.py
+++ sol/database/thumbs.py
@@ -15,9 +15,16 @@ from PIL import Image, ImageChops, ImageOps
 
 class ThumbMaker:
 	def __init__(self,desired_width,aspect_width=16,aspect_height=9):
-		if not os.path.exists('./scrot/'): os.makedirs('./scrot/')
+		if not os.path.exists(C.SCROT_DIR): os.makedirs(C.SCROT_DIR)
 		self.desired_width = desired_width
 		self.desired_height = int(desired_width * aspect_height / aspect_width)
+
+	@property
+	def tempfile(self):
+		return os.path.join(C.SCROT_DIR,'temp.png')
+
+	def correct_ff_path(self,cmd):
+		return os.path.join(C.FFMPEG_PATH,cmd)
 
 	def update_desired_width(self,new_width):
 		self.desired_height = int(self.desired_height / self.desired_width * new_width)
@@ -25,8 +32,8 @@ class ThumbMaker:
 
 	def quit(self):
 		# clean up after yourself
-		if os.path.exists('./scrot/temp.png'):
-			os.remove('./scrot/temp.png')
+		if os.path.exists(self.tempfile):
+			os.remove(self.tempfile)
 
 	def create_shot(self,f_in,f_out,seek_time=0,width=0,height=0):
 		# makes a screenshot (used for when need to make a one-off thumbnail)
@@ -38,32 +45,35 @@ class ThumbMaker:
 			# want total length of video
 			file_info = ''
 			try:
-				file_info = subprocess.check_output('ffprobe -i "{}" -show_entries format=duration -v quiet -of csv="p=0"'.format(f_in), 
+				command = '{} -i "{}" -show_entries format=duration -v quiet -of csv="p=0"'.format(self.correct_ff_path('ffprobe'),f_in)
+				file_info = subprocess.check_output( command, 
 							stderr=subprocess.STDOUT, shell=True)
 			except subprocess.CalledProcessError as e:
 				file_info = e.output
 			file_info = float(file_info.decode('utf-8'))
 			ss_param = seek_time * file_info
-			command = 'ffmpeg -ss {0:.3f} -y -i "{1}" -vf "thumbnail" -q:v 2 -vframes 1 "./scrot/temp.png" -hide_banner -loglevel panic'.format(ss_param, f_in)
+			command = '{0} -ss {1:.3f} -y -i "{2}" -vf "thumbnail" -q:v 2 -vframes 1 "{3}" -hide_banner -loglevel panic'\
+						.format(self.correct_ff_path('ffmpeg'), ss_param, f_in, self.tempfile)
 			print(command)
 			print('#'*40)
 			process = subprocess.Popen(command) # -ss to seek to frame
 			process.communicate() 
-			if os.path.exists('./scrot/temp.png'):
-				self.make_thumbnail('./scrot/temp.png',f_out,
-									 new_size=(width,height))
+			if os.path.exists(self.tempfile):
+				self.make_thumbnail(self.tempfile, f_out,
+									new_size=(width,height))
 				return f_out
 			else:
 				return
 		else:
-			command = 'ffmpeg -y -i "{0}" -vf "thumbnail" -q:v 2 -vframes 1 "./scrot/temp.png" -hide_banner -loglevel panic'.format(f_in)
+			command = '{0} -y -i "{1}" -vf "thumbnail" -q:v 2 -vframes 1 "{2}" -hide_banner -loglevel panic'\
+						.format(self.correct_ff_path('ffmpeg'), f_in, self.tempfile)
 			print(command)
 			print('#'*40)
 			process = subprocess.Popen(command) # -ss to seek to frame
 			process.communicate()
-			if os.path.exists('./scrot/temp.png'):
-				self.make_thumbnail('./scrot/temp.png',f_out,
-									 new_size=(width,height))
+			if os.path.exists(self.tempfile):
+				self.make_thumbnail(self.tempfile, f_out,
+									new_size=(width,height))
 				return f_out
 			else:
 				return
@@ -84,7 +94,7 @@ class ThumbMaker:
 
 		# create output name
 		split_f_name = os.path.split(os.path.splitext(f_name)[0])
-		output_base_name = './scrot/' + split_f_name[1]
+		output_base_name = os.path.join(C.SCROT_DIR,split_f_name[1])
 		bad_hash = self.nice_num(split_f_name[0]) * self.nice_num(split_f_name[1])
 		while True:
 			output_name = output_base_name + "_" + str(bad_hash) + "_0.png"
@@ -99,7 +109,8 @@ class ThumbMaker:
 			# want total length of video
 			file_info = ''
 			try:
-				file_info = subprocess.check_output('ffprobe -i "{}" -show_entries format=duration -v quiet -of csv="p=0"'.format(f_name), 
+				command = '{} -i "{}" -show_entries format=duration -v quiet -of csv="p=0"'.format(self.correct_ff_path('ffprobe'),f_name)
+				file_info = subprocess.check_output(command, 
 							stderr=subprocess.STDOUT, shell=True)
 			except subprocess.CalledProcessError as e:
 				file_info = e.output
@@ -108,7 +119,8 @@ class ThumbMaker:
 			interval = file_info / (n_frames + 1)
 			for i in range(n_frames):
 				seek_time = interval / 2 + i * interval
-				command = 'ffmpeg -ss {0:.3f} -y -i "{1}" -vf "thumbnail" -q:v 2 -vframes 1 "./scrot/temp.png" -hide_banner -loglevel panic'.format(seek_time, f_name)
+				command = '{0} -ss {1:.3f} -y -i "{2}" -vf "thumbnail" -q:v 2 -vframes 1 "{3}" -hide_banner -loglevel panic'\
+							.format(self.correct_ff_path('ffmpeg'), seek_time, f_name, self.tempfile)
 				print(command)
 				print('#'*40)
 				process = subprocess.Popen(command) # -ss to seek to frame
@@ -116,9 +128,9 @@ class ThumbMaker:
 
 				i_output_name = output_name[:-5] + '{}.png'.format(i)
 				print(i_output_name)
-				if os.path.exists('./scrot/temp.png'):
-					self.make_thumbnail('./scrot/temp.png',i_output_name,
-										 new_size=(self.desired_width,self.desired_height))
+				if os.path.exists(self.tempfile):
+					self.make_thumbnail(self.tempfile, i_output_name,
+										new_size=(self.desired_width,self.desired_height))
 					tor += [i_output_name]
 				else:
 					break
@@ -144,11 +156,3 @@ class ThumbMaker:
 		else:
 			thumb = ImageOps.fit(image, new_size, Image.ANTIALIAS, (0.5, 0.5))
 		thumb.save(f_out)
-
-
-if __name__ == '__main__':
-	test_tm = ThumbMaker(192)
-	test_file = "C:\VJ\__clips__\gundam\dxv\gundam bad blue guy kills feds.mov"
-	test_five = test_tm.gen_thumbnail(test_file,5)
-	new_test = test_tm.create_shot(test_file,test_five[1][:-5]+'lp_0.png',0.5)
-	
