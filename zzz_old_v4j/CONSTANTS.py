c3aee7f0d400038365f5bf07f37371beaabd0551
pussinboot <>
2017-04-25 21:22:47 -0400

    dont need this anymore

--- zzz_old_v4j/CONSTANTS.py
+++
@@ -1,6 +0,0 @@
-NO_Q = 8 # number of cue points, basically how many pads you have
-NO_LAYERS = 4 # number of recording layers
-THUMB_W = 125 # width of thumbnail for clip previews
-MAX_SPEEDUP = 9.99
-MIN_SPEEDUP = 3.33
-RESOLUME_SAVE_DIR = "C:\\Users\\Leo\\Documents\\Resolume Arena 5\\compositions"
