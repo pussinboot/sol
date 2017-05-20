568865b3cab0aa32bf361413164b9c6c5ca5c321
pussinboot <>
2017-05-20 19:50:32 -0400

    ok build is almost complet

--- stupid_build.py
+++ stupid_build.py
@@ -3,7 +3,8 @@ import os
 import subprocess
 
 
-def rebuild():
+def rebuild(top_dir):
+    os.chdir(top_dir)
     subprocess.call([sys.executable, 'setup.py', 'install'], shell=True)
 
 
@@ -12,8 +13,12 @@ fname_to_do_what = {
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        rebuild()
+        top_dir = os.path.split(sys.argv[0])[0]
+        rebuild(top_dir)
         fname = os.path.split(sys.argv[1])[1]
 
         if fname in fname_to_do_what:
             fname_to_do_what[fname]()
+        else:
+            pass
+            # subprocess.call(["sol"], shell=True) # not quite.. need to exec inside virtualenv
