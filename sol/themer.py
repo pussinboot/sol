c5726ecc18e5e0437b74eb5b13aa9579ec37978c
pussinboots <>
2017-06-25 00:05:27 -0400

    theming work

--- sol/themer.py
+++ sol/themer.py
@@ -1,5 +1,5 @@
-import importlib
 from tkinter import ttk
+
 
 def hex_to_rgb(hex_val):
     hex_val = hex_val.lstrip('#')
@@ -7,11 +7,14 @@ def hex_to_rgb(hex_val):
         hex_val = hex_val * 2
     return tuple(int(hex_val[i:i + 2], 16) for i in range(0, 6, 2))
 
+
 def rgb_to_hex(rgb_val):
     return '#%02x%02x%02x' % rgb_val
 
+
 def linerp_helper(c1, c2, d):
     return tuple(int((c2[i] - c1[i]) * d + c1[i]) for i in range(3))
+
 
 def linerp_colors(two_colors, n):
     # input is 2 hex vals, outputs n many hex vals between them
@@ -20,10 +23,6 @@ def linerp_colors(two_colors, n):
     from_col, to_col = hex_to_rgb(two_colors[0]), hex_to_rgb(two_colors[1])
     return [rgb_to_hex(linerp_helper(from_col, to_col, (x / (n - 1)))) for x in range(n)]
 
-def config_setup(config):
-    theme_package_name = 'sol.themes.' + config.SELECTED_THEME
-    sel_theme = importlib.import_module(theme_package_name)
-    return sel_theme
 
 def setup(sel_theme):
     ttk.Style().theme_use(sel_theme.default_style)
---
+++ sol/themes/dev/get_names.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+from tkinter import ttk
+s = ttk.Style()
+
+# style defines how subelements of a widget are arranged
+# use this to find what's inside of a widget
+# print(s.layout('TButton'))
+
+# all of the available options 
+# print(s.element_options('TButton.label'))
+
+def ppl(subelem, prefix=""):
+    to_print = prefix + ""
+    to_print += subelem[0] + ' : '
+    opts = []
+    if isinstance(subelem[1], dict):
+        for k, v in subelem[1].items():
+            if k != 'children':
+                opts.append('-' + k + '=' + v)
+        to_print += ', '.join(opts)
+        if 'children' in subelem[1]:
+            for c in subelem[1]['children']:
+                to_print += '\n' + ppl(c, prefix + " " * 4)
+    else:
+        to_print += subelem[1]
+    return to_print
+
+def pretty_print_layout(style_name):
+    print('---', style_name, '---')
+    ugly = s.layout(style_name)
+    for x in ugly:
+        print(ppl(x))
+
+list_of_widgets = [
+# ttk things
+'TLabel',
+'TButton',
+'TFrame',
+'Vertical.TScale',
+'Horizontal.TScale',
+'TEntry',
+'TLabelframe',
+'TCheckbutton',
+'TSeparator',
+'Vertical.TScrollbar',
+'Horizontal.TScrollbar',
+'TNotebook',
+'Treeview',
+'TRadiobutton',
+'TCombobox',
+# regular tk things (can't style these)
+# 'Menubutton', # should be replaced everywhere with combobox
+# 'Canvas',
+# 'Scrollbar',
+# 'Spinbox',
+]
+
+for w in list_of_widgets:
+    pretty_print_layout(w)
+
