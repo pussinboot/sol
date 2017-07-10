563b1f28849d1d2f8cb760e7c02d8fe564bb403c
pussinboots <>
2017-07-10 14:59:47 -0400

    setting up all cmd names

--- sol/inputs/neo_midi_config.py
+++ sol/inputs/neo_midi_config.py
@@ -3,7 +3,14 @@ from tkinter import ttk
 
 import rtmidi
 
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
 import re
+
+
+from sol.config import GlobalConfig
+C = GlobalConfig()
 
 # to-do
 # need toggle for config vs usage
@@ -13,11 +20,15 @@ import re
 # handle errors? what happens on midi disconnect : ( ? nothing..
 
 
+
+
 class MidiInterface:
     def __init__(self):
         self.input_ports = {}
         self.midi_inputs = {}
         self.refresh_input_ports()
+        self.name_to_cmd = {}
+        self.gen_name_to_cmd()
 
     def refresh_input_ports(self):
         # get a list of all inputs
@@ -60,10 +71,87 @@ class MidiInterface:
     def midi_error_fun(self, error, error_msg):
         pass
 
+    def gen_name_to_cmd(self):
+
+        def add_cmd(cmd_array, l=-1, i=-1):
+            [cmd_name, cmd_osc, cmd_desc] = cmd_array
+            if l > -1:
+                cmd_name += '_l{}'.format(l)
+                cmd_osc = cmd_osc.format(l)
+            if i > -1:
+                cmd_name = cmd_name.format(i)
+                cmd_osc += ' {}'.format(i)
+            self.name_to_cmd[cmd_name] = {'addr': cmd_osc, 'desc': cmd_desc}
+
+        # not per layer cmds
+        not_per_layer_cmds = [
+        ['coln_next','/magi/cur_col/select_left', 'select collection to the left'],
+        ['coln_prev','/magi/cur_col/select_right', 'select collection to the right'],
+        ]
+
+        # per layer cmds
+        # timeline control
+        per_layer_cmds = [
+        ['ctrl_start', '/magi/control{}/start', 'start control'],
+        ['ctrl_stop', '/magi/control{}/stop', 'stop control'],
+        ['ctrl_do', '/magi/control{}/do', 'do control'],
+        ['ctrl_sens', '/magi/control{}/sens', 'modify control sensitivity'],
+        # playback
+        ['pb_play','/magi/layer{}/playback/play', 'play'],
+        ['pb_pause','/magi/layer{}/playback/pause', 'pause'],
+        ['pb_reverse','/magi/layer{}/playback/reverse', 'reverse'],
+        ['pb_clear','/magi/layer{}/playback/clear', 'clear'],
+        # speed
+        ['pb_spd','/magi/layer{}/playback/speed', 'set speed'],
+        ['pb_spd_adjust','/magi/layer{}/playback/speed/adjust', 'adjust speed'],
+        ['pb_spd_double','/magi/layer{}/playback/speed/adjust/factor 2', 'increase speed by a factor of 2'],
+        ['pb_spd_halve','/magi/layer{}/playback/speed/adjust/factor 0.5', 'decrease speed by a factor of 2'],
+        # loop points
+        ['pb_lp_on_off','/magi/layer{}/loop/on_off', 'de/activate looping'],
+        ['pb_lp_type','/magi/layer{}/loop/type', 'change loop type'],
+        ['pb_lp_select_next','/magi/layer{}/loop/select/move +1', 'select the next loop range'],
+        ['pb_lp_select_prev','/magi/layer{}/loop/select/move -1', 'select the previous loop range'],
+        ['pb_lp_set_a','/magi/layer{}/loop/set/a', 'set the loop range\'s beginning'],
+        ['pb_lp_set_b','/magi/layer{}/loop/set/b', 'set the loop range\'s end'],
+        ]
+        # cue points
+        qp_cmds = [
+        # x no qp
+        # maybe instead of act/deact have a "press this button so next is delet" thing
+        ['pb_qp_{}_act','/magi/layer{}/cue', 'set/activate cue point'],
+        ['pb_qp_{}_deact','/magi/layer{}/cue/clear', 'clear cue point'],
+        # collections
+        ['coln_select_{}','/magi/cur_col/select_clip/layer{}', 'select a clip from current collection'],
+        ]
+
+        lp_cmds = [
+        # x no lp
+        ['pb_lp_{}_act','/magi/layer{}/loop/select', 'select loop range'],
+        ['pb_lp_{}_deact','/magi/layer{}/loop/clear', 'clear the loop range'],
+        ]
+
+        for c in not_per_layer_cmds:
+            add_cmd(c)
+
+        for l in range(C.NO_LAYERS):
+
+            for c in per_layer_cmds:
+                add_cmd(c, l)
+
+            for c in qp_cmds:
+                for i in range(C.NO_Q):
+                    add_cmd(c, l, i)
+            for c in lp_cmds:
+                for i in range(C.NO_LP):
+                    add_cmd(c, l, i)
+
+        pp.pprint(self.name_to_cmd)
 
 class MidiConfig:
-    def __init__(self, root):
+    def __init__(self, root, parent):
+        # needs to show up to the top right of the main window.
         self.root = root
+        self.parent = parent
         self.midi_int = MidiInterface()
 
         self.main_frame = ttk.Frame(self.root)
@@ -81,12 +169,21 @@ class MidiConfig:
 
         self.choose_midi.pack()
 
-        self.overlay = MidiOverlay(self)
+        self.overlay = MidiOverlay(self.parent)
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
 
     def midi_choice_changed(self, *args):
         new_choice = self.midi_choice.get()
         if new_choice != 'None':
             self.midi_int.open_midi_input(new_choice)
+
+    def close(self):
+        if self.overlay is not None:
+            self.overlay.close()
+        self.root.destroy()
+        # may need to undo topmost
+        # self.parent.root.call('wm', 'attributes', '.', '-topmost', str(int(self.parent.on_top_toggle.get())))
+
 
 
 class MidiOverlay:
@@ -105,6 +202,7 @@ class MidiOverlay:
         self.root.overrideredirect(True)
 
         self.parent.root.bind('<Configure>', self.parent_moved)
+        self.parent_moved()
 
     def parent_moved(self, *args):
         parent_pos = self.parent.root.geometry()
@@ -116,11 +214,14 @@ class MidiOverlay:
                 self.offsets = [self.parent.root.winfo_rootx() - new_dims[2],
                                 self.parent.root.winfo_rooty() - new_dims[3]]
                 self.canvas.configure(width=new_dims[0] + self.offsets[0],
-                                      height=new_dims[1] + 2 * self.offsets[0] + self.offsets[1])
+                                      height=new_dims[1] + self.offsets[1])
                 # redraw everything..
             self.root.geometry('+{}+{}'.format(*new_dims[2:]))
             self.last_pos = parent_pos
 
+    def close(self):
+        self.parent.root.unbind('<Configure>')
+        self.root.destroy()
 
 if __name__ == '__main__':
 
--- tests/scratch.py
+++ tests/scratch.py
@@ -1,14 +1,3 @@
-# from tkinter import ttk
-
-# themes = ttk.Style().theme_names()
-
-# print(themes)
-
-import os, pkgutil
-import sol.themes
-
-pkg_path = os.path.dirname(sol.themes.__file__)
-print(pkg_path)
-
-subm = [name for _, name, _ in pkgutil.walk_packages([pkg_path])]
-print(subm)
+test = ['a', 'b', 'c']
+[cmd_name, cmd_osc, cmd_desc] = test
+print(cmd_name)
